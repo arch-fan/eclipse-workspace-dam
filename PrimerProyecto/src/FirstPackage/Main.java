84ea284f05d05cd44a559d87b7fc7f094b3f9555
Juan Luis Sanchez Pozo <juan.sanchez@aulas.naves>
2023-09-25 17:42:53 +0200

    checkpoint

--- PrimerProyecto/src/FirstPackage/Main.java
+++ PrimerProyecto/src/FirstPackage/Main.java
@@ -1,7 +1,11 @@
 package FirstPackage;
 
 public class Main {
+	
+	static int sueldo = 32000;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Probando Java");
 	}	
 }
