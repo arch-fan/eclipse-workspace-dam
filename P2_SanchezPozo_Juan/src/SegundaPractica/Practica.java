aa881d98f0dd15387ae8ca473fe96b228e2cc8d9
Juan Luis Sanchez Pozo <juan.sanchez@aulas.naves>
2023-10-17 16:40:46 +0200

    Save

--- P2_SanchezPozo_Juan/src/SegundaPractica/Practica.java
+++ P2_SanchezPozo_Juan/src/SegundaPractica/Practica.java
@@ -44,11 +44,15 @@ public class Practica {
     }
 
     // Primer punto
+
     private static int contarPalabras(String texto) {
+        // Separamos por espacios y contamos la longitud del array que se ha creado con
+        // el metodo split.
         return texto.split(" ").length;
     }
 
     private static int contarCaracteres(String texto) {
+        // Contamos la longitud del string a partir del metodo length.
         return texto.length();
     }
 
@@ -56,7 +60,9 @@ public class Practica {
 
         int contador = 0;
 
+        // Recorremos el texto caracter por caracter
         for (char c : texto.toCharArray()) {
+            // Si el caracter del texto es un dígito, sumamos al contador.
             if (Character.isDigit(c)) {
                 contador++;
             }
@@ -66,11 +72,15 @@ public class Practica {
     }
 
     // Segundo punto
+
     private static String encontrarPalabraMasLarga(String texto) {
 
         String palabraMasLarga = "";
 
+        // Recorremos el texto palabra por palabra, separando por espacios.
         for (String palabra : texto.split(" ")) {
+            // Si la palabra guardada en la variable palabraMasLarga es mayor a la siguiente
+            // palabra recorrida, la guardamos en la variable.
             if (palabraMasLarga.length() < palabra.length()) {
                 palabraMasLarga = palabra;
             }
@@ -85,12 +95,15 @@ public class Practica {
     }
 
     // Cuarto punto
+
     private static char[] letraQueNoEstaDelAbc(String texto) {
         char[] abecedario = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
         for (Character c : abecedario) {
             if (texto.contains(c.toString())) {
+                // Utilizamos la propiedad de los chars de poder restarse entre si para poder
+                // conseguir la posicion en la variable abecedario. Y la "borramos".
                 abecedario[c - 'a'] = ' ';
             }
         }
@@ -98,10 +111,16 @@ public class Practica {
         return abecedario;
     }
 
+    // Para obtener de una manera más simple el resultado, he optado por guardar en
+    // 10 posiciones de un array la cantidad de veces que sale un numero. La
+    // posicion 0 guarda las veces que ha salido 0. La posicion 1 guarda todas las
+    // veces que ha salido 1 y asi consecutivamente.
     private static int[] cuantasVecesApareceCadaNumero(String texto) {
+        // Nuevo Array de 10 digitos [0-9]
         int[] digitos = new int[10];
 
         for (char c : texto.toCharArray()) {
+            // Si es digito el caracter, sumamos 1 a la posicion del numero.
             if (Character.isDigit(c)) {
                 int index = Character.getNumericValue(c);
                 digitos[index]++;
@@ -113,11 +132,15 @@ public class Practica {
 
     // Sexto punto
     private static String invertirTexto(String texto) {
+        // Explotamos la funcionalidad de la clase StringBuilder para poder invertir el
+        // string de una manera más simple.
         return new StringBuilder(texto).reverse().toString();
     }
 
     // Séptimo punto
     private static String cambiarVocalesPorI(String texto) {
+        // Con una RegEx encontramos todas las vocales minusculas y las reemplazamos por
+        // 'i' minuscula, y con la otra hacemos lo mismo pero con las mayusculas.
         return texto.replaceAll("[aeou]", "i").replaceAll("[AEOU]", "I");
     }
 }
