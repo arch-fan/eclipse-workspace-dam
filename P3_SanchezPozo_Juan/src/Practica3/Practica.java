c5b2a368e6703ce5bcdc872670269124d09d7200
Juan Luis Sanchez Pozo <juan.sanchez@aulas.naves>
2023-10-23 16:49:49 +0200

    Save

--- P3_SanchezPozo_Juan/src/Practica3/Practica.java
+++ P3_SanchezPozo_Juan/src/Practica3/Practica.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Practica {
-    
+
     private static ArrayList<Paciente> listaPacientes = new ArrayList<>();
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+
         System.out.println("¿Cuantos pacientes vas a introducir?: ");
         int nPacientes = sc.nextInt();
-        
-        for(int i = 1; i <= nPacientes; i++) {
+
+        for (int i = 1; i <= nPacientes; i++) {
             System.out.println("Introduce el nombre del paciente " + i + ": ");
-            sc.next();
-            String nombre = sc.nextLine(); 
+            sc.nextLine();
+            String nombre = sc.nextLine();
 
             System.out.println("Introduce el peso del paciente (en kg) " + i + ": ");
             double peso = sc.nextDouble();
@@ -25,13 +25,22 @@ public class Practica {
             double altura = sc.nextDouble();
 
             listaPacientes.add(new Paciente(nombre, peso, altura));
+
         }
-        
-        listaPacientes.forEach( paciente -> {
-            System.out.println(paciente.getAltura());
-        });
-        
+
         sc.close();
+    }
+
+    private static Paciente getPacienteMasBajo(ArrayList<Paciente> listaPaciente) {
+        Paciente pacienteMasBajo = listaPaciente.get(0); // Asumimos que el primer paciente es el más bajo inicialmente
+
+        for (Paciente paciente : listaPaciente) {
+            if (paciente.getAltura() < pacienteMasBajo.getAltura()) {
+                pacienteMasBajo = paciente; // Encontramos un paciente más bajo
+            }
+        }
+
+        return pacienteMasBajo;
     }
 
 }
