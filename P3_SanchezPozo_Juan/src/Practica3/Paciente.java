6b9b62ee955894b363ad3bc153a4a8e62bf8be82
Juan Luis Sanchez Pozo <juan.sanchez@aulas.naves>
2023-10-26 17:09:10 +0200

    Save

--- P3_SanchezPozo_Juan/src/Practica3/Paciente.java
+++ P3_SanchezPozo_Juan/src/Practica3/Paciente.java
@@ -1,14 +1,34 @@
 package Practica3;
 
+import java.util.ArrayList;
+
 public class Paciente {
     private String nombre;
     private double peso;
     private double altura;
+    private int dieta = 0;
 
     public Paciente(String nombre, double peso, double altura) {
         this.nombre = nombre;
         this.peso = peso;
         this.altura = altura;
+    }
+
+    public static ArrayList<Paciente> generarPacientesPrueba() {
+        ArrayList<Paciente> pacientes = new ArrayList<>();
+
+        pacientes.add(new Paciente("Paco", 70.5, 1.75));
+        pacientes.add(new Paciente("María", 65.2, 1.68));
+        pacientes.add(new Paciente("Carlos", 80.0, 1.80));
+        pacientes.add(new Paciente("Laura", 55.7, 1.60));
+        pacientes.add(new Paciente("Pedro", 90.3, 1.90));
+        pacientes.add(new Paciente("Luisa", 75.1, 1.72));
+        pacientes.add(new Paciente("Ana", 60.8, 1.65));
+        pacientes.add(new Paciente("Miguel", 85.6, 1.78));
+        pacientes.add(new Paciente("Sofía", 58.9, 1.63));
+        pacientes.add(new Paciente("David", 77.4, 1.76));
+
+        return pacientes;
     }
 
     public String getNombre() {
@@ -26,5 +46,12 @@ public class Paciente {
     public double calcularIMC() {
         return peso / Math.pow(altura, 2);
     }
-}
 
+    public int getDieta() {
+        return dieta;
+    }
+
+    public void setDieta(int dieta) {
+        this.dieta = dieta;
+    }
+}
--- P3_SanchezPozo_Juan/src/Practica3/Practica.java
+++ P3_SanchezPozo_Juan/src/Practica3/Practica.java
@@ -1,38 +1,66 @@
 package Practica3;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.Map;
-import java.util.List;
 
 public class Practica {
 
-    private static ArrayList<Paciente> listaPacientes = new ArrayList<>();
+    private static ArrayList<Paciente> listaPacientes = Paciente.generarPacientesPrueba(); // new ArrayList<>();
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        System.out.println("¿Cuantos pacientes vas a introducir?: ");
-        int nPacientes = sc.nextInt();
+//        System.out.println("¿Cuantos pacientes vas a introducir?: ");
+//        int nPacientes = sc.nextInt();
+//
+//        for (int i = 1; i <= nPacientes; i++) {
+//            System.out.println("Introduce el nombre del paciente " + i + ": ");
+//            sc.nextLine();
+//            String nombre = sc.nextLine();
+//
+//            System.out.println("Introduce el peso del paciente (en kg) " + i + ": ");
+//            double peso = sc.nextDouble();
+//
+//            System.out.println("Introduce la altura del paciente (en metros) " + i + ": ");
+//            double altura = sc.nextDouble();
+//
+//            listaPacientes.add(new Paciente(nombre, peso, altura));
+//
+//        }
 
-        for (int i = 1; i <= nPacientes; i++) {
-            System.out.println("Introduce el nombre del paciente " + i + ": ");
-            sc.nextLine();
-            String nombre = sc.nextLine();
+        System.out.println("El paciente mas bajo es " + getPacienteMasBajo(listaPacientes).getNombre());
+        System.out.println("El paciente con más peso es " + getPacienteMasPesado(listaPacientes).getNombre());
 
-            System.out.println("Introduce el peso del paciente (en kg) " + i + ": ");
-            double peso = sc.nextDouble();
+        Paciente pacienteIMCmasAlto = getPacienteIMCMasAlto(listaPacientes);
+        Paciente pacienteIMCmasBajo = getPacienteIMCMasBajo(listaPacientes);
 
-            System.out.println("Introduce la altura del paciente (en metros) " + i + ": ");
-            double altura = sc.nextDouble();
+        System.out.println("El paciente con el IMC mas alto es " + pacienteIMCmasAlto.getNombre() + " con un IMC de "
+                + redondearDecimales(pacienteIMCmasAlto.calcularIMC()));
+        System.out.println("El paciente con el IMC mas bajo es " + pacienteIMCmasBajo.getNombre() + " con un IMC de "
+                + redondearDecimales(pacienteIMCmasBajo.calcularIMC()));
 
-            listaPacientes.add(new Paciente(nombre, peso, altura));
+        System.out.println("La media de altura es " + getMediaAltura(listaPacientes) + " y la mediana es "
+                + getMedianaAltura(listaPacientes));
+        System.out.println("La media de peso es " + getMediaPeso(listaPacientes) + " y la mediana es "
+                + getMedianaPeso(listaPacientes));
 
-        }
+        clasificarPacientes(listaPacientes).forEach((peso, cantidad) -> {
+            System.out.println("Hay " + cantidad + " con " + peso);
+        });
+
+        setDietas(listaPacientes);
+        listaPacientes.forEach(paciente -> {
+            System.out.println(paciente.getNombre() + " debe cumplir con una dieta de " + paciente.getDieta() + "kcal");
+        });
 
         sc.close();
+    }
+
+    private static double redondearDecimales(double n) {
+        return Math.round(n * 100.0) / 100.0;
     }
 
     private static Paciente getPacienteMasBajo(ArrayList<Paciente> listaPaciente) {
@@ -83,27 +111,28 @@ public class Practica {
         return pacienteIMCMasBajo;
     }
 
-    private static double getAlturaMedia(ArrayList<Paciente> listaPaciente) {
+    private static double getMediaAltura(ArrayList<Paciente> listaPaciente) {
         double alturasSumadas = 0;
 
         for (Paciente paciente : listaPaciente) {
             alturasSumadas += paciente.getAltura();
         }
 
-        return alturasSumadas / listaPaciente.size();
+        return redondearDecimales(alturasSumadas / listaPaciente.size());
     }
 
-    private static double getPesoMedio(ArrayList<Paciente> listaPaciente) {
+    private static double getMediaPeso(ArrayList<Paciente> listaPaciente) {
         double pesosSumados = 0;
 
         for (Paciente paciente : listaPaciente) {
             pesosSumados += paciente.getPeso();
         }
 
-        return pesosSumados / listaPaciente.size();
+        return redondearDecimales(pesosSumados / listaPaciente.size());
     }
 
     private static double calcularMediana(ArrayList<Double> datos) {
+        Collections.sort(datos);
         int longitudDatos = datos.size();
         double mediana;
 
@@ -125,17 +154,17 @@ public class Practica {
             alturaPacientes.add(paciente.getAltura());
         }
 
-        return calcularMediana(alturaPacientes);
+        return redondearDecimales(calcularMediana(alturaPacientes));
     }
 
     private static double getMedianaPeso(ArrayList<Paciente> listaPaciente) {
         ArrayList<Double> pesoPacientes = new ArrayList<>();
 
         for (Paciente paciente : listaPaciente) {
-            pesoPacientes.add(paciente.getAltura());
+            pesoPacientes.add(paciente.getPeso());
         }
 
-        return calcularMediana(pesoPacientes);
+        return redondearDecimales(calcularMediana(pesoPacientes));
     }
 
     public static Map<String, Integer> clasificarPacientes(ArrayList<Paciente> listaPaciente) {
@@ -144,53 +173,44 @@ public class Practica {
         int pesoNormal = 0;
         int sobrepeso = 0;
         int obesidad = 0;
-        
-       for(Paciente paciente : listaPaciente) {
-           double imc = paciente.calcularIMC();
-           
-           if(imc < 18.5) {
-               pesoBajo++;
-           } else if(imc < 25.0) {
-               pesoNormal++;
-           } else if(imc < 40.0) {
-               sobrepeso++;
-           } else {
-               obesidad++;
-           }
-       }
-       
-       clasificacion.put("Peso Bajo", pesoBajo);
-       clasificacion.put("Peso Normal", pesoNormal);
-       clasificacion.put("Sobrepeso", sobrepeso);
-       clasificacion.put("Obesidad", obesidad);
-       
-       return clasificacion;
-        
-    }
- 
-    public static Map<String, String> getListaDietas(ArrayList<Paciente> listaPaciente) {
-        
-        Map<Paciente, String> listaDietas = new HashMap<>();
-        
-        for(Paciente paciente : listaPacientes) {
-            String dieta = "";
+
+        for (Paciente paciente : listaPaciente) {
             double imc = paciente.calcularIMC();
-            
-            if(imc < 18.5) {
+
+            if (imc < 18.5) {
                 pesoBajo++;
-            } else if(imc < 25.0) {
+            } else if (imc < 25.0) {
                 pesoNormal++;
-            } else if(imc < 40.0) {
+            } else if (imc < 40.0) {
                 sobrepeso++;
             } else {
                 obesidad++;
             }
-            
-            
-            
-            listaDietas.put(paciente.getNombre(), dieta);
+        }
+
+        clasificacion.put("Peso Bajo", pesoBajo);
+        clasificacion.put("Peso Normal", pesoNormal);
+        clasificacion.put("Sobrepeso", sobrepeso);
+        clasificacion.put("Obesidad", obesidad);
+
+        return clasificacion;
+
+    }
+
+    public static void setDietas(ArrayList<Paciente> listaPaciente) {
+        for (Paciente paciente : listaPaciente) {
+            double imc = paciente.calcularIMC();
+
+            if (imc < 18.5) {
+                paciente.setDieta(3000);
+            } else if (imc < 25.0) {
+                paciente.setDieta(2500);
+            } else if (imc < 40.0) {
+                paciente.setDieta(2000);
+            } else {
+                paciente.setDieta(1500);
+            }
         }
     }
 
-    
 }
