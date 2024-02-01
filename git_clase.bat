937c68029860b61f3c637c689b7c77fecedbc401
Juan Luis Sanchez Pozo <juan.sanchez@aulas.naves>
2024-02-01 16:56:58 +0100

    Save

--- git_clase.bat
+++
@@ -1,4 +0,0 @@
-
-"D:\Usuarios\juan.sanchez\AppData\Local\Programs\Git\bin\git.exe" add .
-"D:\Usuarios\juan.sanchez\AppData\Local\Programs\Git\bin\git.exe" commit -m "Save"
-"D:\Usuarios\juan.sanchez\AppData\Local\Programs\Git\bin\git.exe" push origin main
